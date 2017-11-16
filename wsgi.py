144d3b2269a611e211a5de2ce689d4ee3580c5b2
Flok <fk@mxu.de>
2017-11-16 08:26:35 +0100

    replaced more app with application

--- wsgi.py
+++ wsgi.py
@@ -7,15 +7,15 @@ languages = [{'cid': '1-2345-67', 'value': 'Active'}, {'cid': '2-2345-67', 'valu
              {'cid': '5-2345-67', 'value': 'Active'}, {'cid': '6-2345-67', 'value': 'Active'},
              {'cid': '7-2345-67', 'value': 'Active'}]
 
-@app.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def default():
     return jsonify({'message': 'It works!'})
 
-@app.route('/customers', methods=['GET'])
+@application.route('/customers', methods=['GET'])
 def getList():
     return jsonify({'customers': languages})
 
-@app.route('/customers/<string:cid>', methods=['GET'])
+@application.route('/customers/<string:cid>', methods=['GET'])
 def getOne(isoParam):
     customers = [customer for customer in customers if customer['cid'] == cid]
     return jsonify({'customer': customers[0]})
